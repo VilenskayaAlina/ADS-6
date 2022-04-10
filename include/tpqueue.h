ce80b5f3c51868faa871e5666a55bc6ef1ddb5b5
VilenskayaAlina <99177250+VilenskayaAlina@users.noreply.github.com>
2022-04-10 23:54:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,32 +5,39 @@
 template<typename T, int size>
 class TPQueue {
  private:
-  T arr[size];
+  T arr[size] = {0};
   int first, last;
   }
 
  public:
   TPQueue():first(0), last(0) {}
 
-  void push(const T& value) {
-    if (fast - first >= size)
+  void push(T& value) {
+    int i = last;
+    if (fast - first >= size) {
       throw std::string("Full!");
-    else
-      arr[(last++) % size] = value;
+    } else {
+        int i = last;
+        while ((value.prior > arr[i % size].prior) && (i-- >= first)) {
+          arr[(i + 1) % size] = arr[i % size];
+          i--;
+        }
+     arr[(i + 1) % size] = value;
     }
+  }
   T pop() {
     if (first == last)
       trow std::string("Empty!");
     else
       return arr[(first++) % size];
     }
-  T front() const {
+  T front() {
     if (first == last)
       trow std::string("Empty!");
     else
       return arr[first % size];
   }
-  T back() const {
+  T back() {
     if (first == last)
       trow std::string("Empty!");
     else
